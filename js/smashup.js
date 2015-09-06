7b67f5d3af7136d159e724b0ae5f702d8d204624
Jack Truong <jack.truong@uwaterloo.ca>
2015-09-06 10:17:43 -0400

    Added 3rd faction choice

--- js/smashup.js
+++ js/smashup.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     var players =   [
-                        {name:'', factions:['','']},
-                        {name:'', factions:['','']},
-                        {name:'', factions:['','']},
-                        {name:'', factions:['','']}
+                        {name:'', factions:['','','']},
+                        {name:'', factions:['','','']},
+                        {name:'', factions:['','','']},
+                        {name:'', factions:['','','']}
                     ];
     var factions = [];
     factions.push({'name': 'Core Set', 'factions': ['Pirates', 'Ninja', 'Zombies', 'Robots', 'Dinosaurs', 'Wizards', 'Tricksters', 'Aliens']});
@@ -13,6 +13,7 @@ $(document).ready(function() {
     factions.push({'name': 'Big Geeky Box', 'factions': ['The Geeks']});
     factions.push({'name': 'Monster Smash', 'factions': ['Vampires', 'Mad Scientists', 'Giant Ants', 'Werewolves']});
     factions.push({'name': 'Pretty Pretty Smash Up', 'factions': ['Fairies', 'Mythic Horses', 'Kitty Cats', 'Princesses']});
+    //factions.push({'name': 'Munchkin', 'factions': ['Warriors', 'Clerics', 'Mages', 'Rogues', 'Orcs', 'Dwarves', 'Elves', 'Halflings']});
 
     var make_id = function(string) {
         return string.toLowerCase().replace(/[^a-z]+/g, '_');
@@ -57,25 +58,25 @@ $(document).ready(function() {
 
     // http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
     function shuffle(array) {
-    var currentIndex = array.length
-      , temporaryValue
-      , randomIndex
-      ;
+        var currentIndex = array.length
+          , temporaryValue
+          , randomIndex
+          ;
 
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
 
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
 
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
 
-    return array;
+        return array;
     }
 
     $('#randomize').click(function() {
@@ -83,11 +84,12 @@ $(document).ready(function() {
         $('#not-enough-factions').hide();
         $('#not-enough-players').hide();
         $('#scoring > div').hide();
-        players =   [{name:'', factions:['','']}, {name:'', factions:['','']}, {name:'', factions:['','']}, {name:'', factions:['','']} ];
+        players =   [{name:'', factions:['','','']}, {name:'', factions:['','','']}, {name:'', factions:['','','']}, {name:'', factions:['','','']} ];
         var dup = $('#duplicate_faction').filter(':checked').length;
+        var three = $('#three_teams').filter(':checked').length;
         var fac = $('.faction-choice').filter(':checked').length;
         var numPlayer = $('.team-name').filter(function() { return $(this).val() !== ''; }).length;
-        if(fac + dup * fac < numPlayer * 2) {
+        if(fac + dup * fac < numPlayer * (2 + three)) {
             $('#not-enough-factions').show();
         } else if(numPlayer < 2) {
             $('#not-enough-players').show();
@@ -100,7 +102,7 @@ $(document).ready(function() {
             var impossible = true;
             while(impossible) {
                 var actualNumFactions = fac + fac * dup;
-                console.log('loop');
+                //console.log('loop');
                 impossible = false;
                 var listOfFactions = [];
                 for(i = 0; i <= dup; i++) {
@@ -129,8 +131,29 @@ $(document).ready(function() {
                     }
                     players[index].factions[1] = listOfFactions.splice(rand,1)[0];
                     actualNumFactions--;
+                    
+                    // 3rd
+                    if(!impossible && three) {
+                        while(true) {
+                            rand = Math.floor((Math.random() * actualNumFactions));
+                            // if the last choice is the same as the other choice, repeat the whole thing
+                            if((listOfFactions[rand] == players[index].factions[0] || listOfFactions[rand] == players[index].factions[1]) && actualNumFactions == 1) {
+                                impossible = true;
+                                break;
+                            }
+                            if(listOfFactions[rand] !== players[index].factions[0] && listOfFactions[rand] !== players[index].factions[1]) {
+                                break;
+                            }
+                        }
+                        players[index].factions[2] = listOfFactions.splice(rand,1)[0];
+                        actualNumFactions--;
+                    }
                     $('#scoring > div:eq('+ix+') .panel-heading').html(players[index].name);
-                    $('#scoring > div:eq('+ix+') .panel-body').html(players[index].factions[0] + ' / ' + players[index].factions[1]);
+                    if(three > 0) {
+                        $('#scoring > div:eq('+ix+') .panel-body').html(players[index].factions[0] + ' / ' + players[index].factions[1] + ' / ' + players[index].factions[2]);
+                    } else {
+                        $('#scoring > div:eq('+ix+') .panel-body').html(players[index].factions[0] + ' / ' + players[index].factions[1]);
+                    }
                 });
             }
             $('#scoring > div:lt('+numPlayer+')').show();
