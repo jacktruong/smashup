0d9999436f9db2b49202467a5cb6c45550992374
Jack Truong <jack.truong@uwaterloo.ca>
2016-09-16 23:07:39 -0400

    Added two factions.

--- js/smashup.js
+++ js/smashup.js
@@ -14,6 +14,8 @@ $(document).ready(function() {
     factions.push({'name': 'Monster Smash', 'factions': ['Vampires', 'Mad Scientists', 'Giant Ants', 'Werewolves']});
     factions.push({'name': 'Pretty Pretty Smash Up', 'factions': ['Fairies', 'Mythic Horses', 'Kitty Cats', 'Princesses']});
     factions.push({'name': 'Munchkin', 'factions': ['Warriors', 'Clerics', 'Mages', 'Rogues', 'Orcs', 'Dwarves', 'Elves', 'Halflings']});
+    factions.push({'name': 'It\'s Your Fault', 'factions': ['Dragons', 'Mythic Greeks', 'Superheroes', 'Sharks']});
+    factions.push({'name': 'Cease & Desist', 'faction': ['Astroknights', 'Star Roamers', 'Changerbots', 'Ignobles']});
 
     var make_id = function(string) {
         return string.toLowerCase().replace(/[^a-z]+/g, '_');
